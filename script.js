c4271d8d2f7b4e2f80f2e5eba402d3251af1ae37
23nrj1997-commits <23nrj1997@gmail.com>
2026-09-08 17:04:22 -0500

    html updated with javascript

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const moviesWrapper = document.querySelector(".movies")
+
 const nameWrapper = document.querySelector(".searchName")
 
 async function getMovies(searchTerm) {
   const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=b8073ce3&s=${searchTerm}`);
+  // nameWrapper.innerHTML = searchTerm;
   const data = await response.json();
-  nameWrapper.innerHTML = searchTerm;
   console.log(data.Search);
   moviesWrapper.innerHTML = data.Search.map((movie) => 
     { return
       `<div class="movie">
         <img src="${movie.Poster}" alt="Poster">
-        <h3>${movie.Title}</h3>
-        <p>Year: ${movie.Year}</p>
+        <h2>${movie.Title}</h2>
+        <h4>Year: ${movie.Year}</h4>
         <button>Learn More</button>
       </div>`
     })
-    .slice(0, 6)
-    .join("");
 }
 
 function onSearchChange(event) {
