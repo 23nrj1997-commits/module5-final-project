b300bff22010dfe5f0a8fac284961a59cf28ed64
23nrj1997-commits <23nrj1997@gmail.com>
2026-09-10 17:25:36 -0500

    javascript adjustment

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const moviesWrapper = document.querySelector(".movies")
 
 const nameWrapper = document.querySelector(".searchName")
 
+let movies = [];
 
 async function getMovies(searchTerm) {
   const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=b8073ce3&s=${searchTerm}`);
   nameWrapper.innerHTML = searchTerm;
   const data = await response.json();
+  movies = data.Search || [];
   console.log(data.Search);
   moviesWrapper.innerHTML = data.Search.map((movie) => 
     { return `<div class="movie">
@@ -25,17 +27,15 @@ function onSearchChange(event) {
 }
 
 function filterMovies(event) {
-  renderMovies(event.target.value);
-}
+    const sortedMovies = [...movies];
 
-function renderMovies(filter) {
-  const movies = document.querySelector(".movies");
+    if (event.target.value === "OLD_TO_NEW") {
+        sortedMovies.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
+    }
 
-  const moviesArray = moviesWrapper.innerHTML;
+    if (event.target.value === "NEW_TO_OLD") {
+        sortedMovies.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+    }
 
-  if (filter === "OLD_TO_NEW") {
-    console.log(filter);
-    const filteredMovies = moviesArray.sort((a, b) => a.year - b.year);
-    console.log(filteredMovies);
-  }
+    renderMovies(sortedMovies);
 }
